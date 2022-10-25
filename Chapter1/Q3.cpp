7959645b38e59567b43ca92e17e2516ba4f5d1b2
Amirmohammad Zarif <Developerboys@gmail.com>
2022-10-26 01:43:19 +0330

    Fixed Q3

--- Chapter1/Q3.cpp
+++ Chapter1/Q3.cpp
@@ -3,14 +3,11 @@
 #include <iostream>
 using namespace std;
 int F(int n){
-    if(n <= 1) return n + 1;
-    else return F(n - 1) + F(n - 2);
+    if(n == 1) return n + 1;
+    else return F(n - 1) + (n - 2);
 }
 
 int main() {
     cout << F(6);
-//    for(int i = 0; i < 10; i++){
-//        cout << F(i)  << endl;
-//    }
     return 0;
 }
