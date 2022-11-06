e3bda067e5c3b28c8957e248d55db3f16993c1f2
Amirmohammad Zarif <Developerboys@gmail.com>
2022-11-07 01:46:50 +0330

    Tests

--- Chapter1/Q7.cpp
+++ Chapter1/Q7.cpp
@@ -1,16 +1,18 @@
 // Created by @amirmohammadzarif
 // Thursday, Oct 20, 2022
 #include <iostream>
+
 using namespace std;
 int C(int k, int n){
     if(k == n || k == 0){
         return 1;
     }else{
+
         return C(k, n - 1 ) + C(k - 1, n - 1);
     }
 }
 
 int main() {
-    cout << C(3, 4);
+    cout << C(3, 8) ;
     return 0;
 }
